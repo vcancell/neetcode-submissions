136359e3148cf2e1e8c8555a069f81802ea52a55
vcancell <161246200+vcancell@users.noreply.github.com>
2026-09-24 12:27:40 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join([char for char in s if char.isalnum()]).lower().replace(" ", "")
+        stack = []
+        l = len(s)
+        if l == 1:
+            return True
+        for i in range(l):
+            c = s[i]
+            if not stack or (stack[-1] != c and (l % 2 != 1 or l // 2 != i)):
+                stack.append(c)
+            elif stack[-1] == c:
+                stack.pop()
+        return not stack
